b8e8a694996bf8c4ef98403d66fa9457436047aa
SuHwanYoon <ehtldy@naver.com>
2024-06-08 18:52:03 +0900

    add Post Method

--- src/main/java/com/yoon/rest/webservices/restful_web_services/user/UserDaoService.java
+++ src/main/java/com/yoon/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -3,6 +3,7 @@ package com.yoon.rest.webservices.restful_web_services.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
@@ -10,15 +11,34 @@ import org.springframework.stereotype.Component;
 public class UserDaoService {
 
 	private static List<User> users = new ArrayList<>();
+	
+	private static int usersCount = 0;
 
 	static {
-		users.add(new User(1, "Yoon", LocalDate.now().minusYears(32)));
-		users.add(new User(2, "Kim", LocalDate.now().minusYears(20)));
-		users.add(new User(3, "Park", LocalDate.now().minusYears(10)));
+		users.add(new User(++usersCount, "Yoon", LocalDate.now().minusYears(32)));
+		users.add(new User(++usersCount, "Kim", LocalDate.now().minusYears(20)));
+		users.add(new User(++usersCount, "Park", LocalDate.now().minusYears(10)));
 	}
 	
+	//모든 유저를 찾는 서비스 메서드
 	public List<User> findAll(){
 		return users;
+	}
+	
+	//새로운 유저를 생성하는 서비스메서드
+	public User saveUser(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+	
+	
+	//int id 로 하나의 유저를 찾는 메서드
+	public User findOne(int id) {
+		//1. users List의 필터기준이 되는 filteringOneUser 변수는 User 객체에서 id 취득후 입력된 @PathVariable id 와 비교
+		Predicate<? super User> filteringOneUser = user -> user.getId().equals(id) ;
+		//2. 필터링된 filteringOneUser 변수에서 findFirst로 Optional type의 User를 찾고 get으로 가져온다
+		return users.stream().filter(filteringOneUser).findFirst().get();
 	}
 	
 }
--- src/main/java/com/yoon/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/yoon/rest/webservices/restful_web_services/user/UserResource.java
@@ -3,6 +3,9 @@ package com.yoon.rest.webservices.restful_web_services.user;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +18,22 @@ public class UserResource {
 		this.userDaoService = userDaoService;
 	}
 	
+	//모든 User를 반환하는 컨트롤러 메서드
 	@GetMapping("/users")
 	public List<User> retriveAllUsers(){
 		return userDaoService.findAll();
+	}
+
+	//@PathVariable로 찾을 path 변수이름은 id
+	@GetMapping("/users/{id}")
+	public User retriveOneUsers(@PathVariable int id){
+		return userDaoService.findOne(id);
+	}
+	
+	//새로운 User객체를 생성요청하는 컨트롤러 메서드
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		userDaoService.saveUser(user);
 	}
 	
 	
