f8dd72a161339f1eb04325235f14c03e831b4af9
SuHwanYoon <ehtldy@naver.com>
2024-07-09 19:48:08 +0900

    add Setting to AWS BeanStalk Deploy

--- src/main/java/com/yoon/rest/webservices/restful_web_services/security/SpringSecurityConfiguration.java
+++ src/main/java/com/yoon/rest/webservices/restful_web_services/security/SpringSecurityConfiguration.java
@@ -16,21 +16,24 @@ public class SpringSecurityConfiguration {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-		//1. 모든 http요청을 인증받도록 설정
+		// 1. 모든 http요청을 인증받도록 설정
 		// 기본 로그인 web view 대신 오류페이지가 나온다
-		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+		http.authorizeHttpRequests(auth -> auth
+																	//AWS배포 Health Check 를 위해 root URL 접근 허용
+																	.requestMatchers("/").permitAll()
+																	.anyRequest().authenticated());
 
 		// 2. 만약 요청이 인증되지않았다면 인증을 위한 브라우저 기본 팝업창(JS의 prompt형식)을 보여준다
 		http.httpBasic(withDefaults());
 
-		//3. CSRF 를 해제하고 RestAPI  POST, PUT 요청을 가능하게 설정
+		// 3. CSRF 를 해제하고 RestAPI POST, PUT 요청을 가능하게 설정
 
 		// http.csrf().disable(); // SB 3.1.x 부터는 추천되지 않는 방식
-		
+
 		// SB 3.1.x 부터는 Lambda식을 사용하거나
-		http.csrf(csrf -> csrf.disable()); 
+		http.csrf(csrf -> csrf.disable());
 		// 메서드참조 :: 를 사용하는 방식이 추천된다
-		// http.csrf(AbstractHttpConfigurer::disable); 
+		// http.csrf(AbstractHttpConfigurer::disable);
 
 		return http.build();
 	}
